2766f753e0f532d925d4230c388ad569634c0a42
VictorGlass <enginemaster551@gmail.com>
2024-09-10 13:48:28 -0300

    Agregando el dia 2

---
+++ 01_Variables.py
@@ -0,0 +1,40 @@
+'''
+Dia 1 - Hola Mundo.
+
+1. Cree variables para su nombre. edad y color favorito e imprimalas..
+'''
+
+#SOLUCION:
+
+mi_nombre = input("Hola!, por favor ingresa tu nombre: ")
+mi_edad = int(input("Perfecto!, ahora ingresa tu edad: "))
+mi_color_favorito = input("Muy bien, y dime ¿Cuál es tu color favorito?: ")
+
+print()
+print(f"Bienvenido {mi_nombre}, tu edad es {mi_edad}, ademas nos dijiste que tu color favorito es el {mi_color_favorito}")
+
+
+#Datos de Python:
+'''
+1. Aprender los terminos: inicializacion, declaracion y asignacion de variables.
+2. Conozca la diferencia entre las variable de tipo dinamico y las de tipo estatico.
+'''
+
+#SOLUCION:
+
+
+#1.
+'''
+INICIALIZACION:
+Esta ocurre cuando se le asigna un valor a una variable por primera vez.
+En Python, la declaracion y la inicializacion suelen suceder al mismo tiempo.
+
+DECLARACION:
+En Python, no se necesita una declaracion explicita como en otros lenguajes de programacion.
+Una variable se declara simplemente al asignarle un valor por primera vez.
+
+ASIGNACION DE VARIABLES:
+Esto se refiere al proceso de darle un valor.
+Puede ocurrir tanto en la inicializacion como en cualquier punto posterior cuando 
+se le asigna un nuevo valor a la variable.
+'''
