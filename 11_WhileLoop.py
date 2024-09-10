558014badbd09c8c9133c80b22b02a0c9bc32e04
VictorGlass <enginemaster551@gmail.com>
2024-09-10 14:29:37 -0300

    Agregando el dia 11

---
+++ 11_WhileLoop.py
@@ -0,0 +1,14 @@
+'''
+Dia 11 - Bucle While.
+
+1. Utilice un bucle while para imprimir numeros pares del 2 al 20. 
+'''
+
+#SOLUCION:
+
+#Declarar la variable
+num = 2
+
+while num <= 20: #Mientras num sea menor que 20
+    print(num)   #Imprime la variable
+    num += 2     #Y se incrementara de a 2 en 2
