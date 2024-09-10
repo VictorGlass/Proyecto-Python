620d5c05e6e1ec67e65eacb4e0480186fc42e06c
VictorGlass <enginemaster551@gmail.com>
2024-09-10 13:42:42 -0300

    Creando el Proyecto

---
+++ 00_HolaMundo.py
@@ -0,0 +1,67 @@
+'''
+Dia 1 - Hola Mundo.
+
+1. Escribe un programa que imprima ¡Hola Mundo! en la consola.
+2. Agregue comentarios de una sola linea y varias lineas de codigo.
+'''
+
+#SOLUCION:
+
+#1.
+
+print("¡Hola Mundo! ")
+
+#2.
+
+#Este es un comentario de una sola linea.
+'''
+Este es un
+comentario de
+varias
+lineas en
+Python.
+'''
+
+#Datos de Python:
+'''
+
+1. ¿Como se compila y se ejecuta Python el codigo?.
+2. Aprende la diferencia entre Python y Java.
+3. Diferencia entre compilador e interprete.
+'''
+
+#SOLUCION:
+
+#1.
+'''
+Python seejecuta en tres pasos:
+1. Interpretacion: El interprete lee el codigo fuente.
+2. Compilacion de Butcode: Convierte el codigo a una forma intermedia(bytecode).
+3. Ejecucion: La maquina virtual de Python ejecuta el bytecode.
+
+** NO NECESITA COMPILADO MANUALMENTE / EL INTERPRETE LO HACE AUTOMATICAMENTE.
+'''
+
+#2.
+'''
+**Python: 
+Interpretado, sintaxis sencilla, dinamicamente tipado. 
+Usado en desarrollo web, ciencia de datos y automatizacion. 
+Mas lento pero facil de aprender.
+
+**Java:
+Compilado, sintaxis mas detallada, estaticamente tipado.
+Usado en aplicaciones empresariales y Android.
+Mas rapido y robusto, pero mas complejo.
+'''
+
+#3.
+'''
+**Compilador:
+Traduce todo el codigo fuente a un achivo ejecutable antes de correr el programa.
+**Ejemplo: C, Java.
+
+**Interprete:
+Traduce y ejecuta el codigo linea por linea en tiempo real.
+**Ejemplo: Python, JavaScript.
+'''
